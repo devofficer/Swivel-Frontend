aa8c499beb45c24adad7623ed3e5567597361a2e
devofficer <myeyesontech@gmail.com>
2022-11-03 07:01:44 -0700

    init: initial structure

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+/**
+ * This is your entrypoint.
+ *
+ * Add your imports and any startup logic here. This file will be imported by
+ * `index.html` and is the entry point for the rollup configuration.
+ */
+
+console.log('app started...');
